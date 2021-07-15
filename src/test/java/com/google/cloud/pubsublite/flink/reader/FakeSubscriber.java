24b8e7efe725dd9a8489aecb2337b4f3c52d6b8d
palmere-google <68394592+palmere-google@users.noreply.github.com>
2021-07-15 17:33:39 -0400

    feat: create the MessageSplitReader (#10)
    * MessageSplitReader
    
    * remove static subclass
    
    * address comments
    
    * add todo

---
+++ src/test/java/com/google/cloud/pubsublite/flink/reader/FakeSubscriber.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.pubsublite.flink.reader;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.api.core.SettableApiFuture;
+import com.google.cloud.pubsublite.SequencedMessage;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class FakeSubscriber implements CompletablePullSubscriber {
+  private final Queue<Optional<SequencedMessage>> messages;
+
+  public FakeSubscriber(Collection<Optional<SequencedMessage>> messages) {
+    this.messages = new LinkedList<>(messages);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return messages.isEmpty();
+  }
+
+  @Override
+  public ApiFuture<Void> onData() {
+    if (messages.isEmpty()) {
+      return SettableApiFuture.create();
+    }
+    return ApiFutures.immediateFuture(null);
+  }
+
+  @Override
+  public Optional<SequencedMessage> messageIfAvailable() {
+    if (messages.isEmpty()) {
+      return Optional.empty();
+    }
+    return messages.poll();
+  }
+
+  @Override
+  public void close() {}
+}
