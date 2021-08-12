5863eb20675b0b8d2d59f87b3b0b579ffaf5f296
palmere <palmere@google.com>
2021-08-11 21:38:37 -0400

    sink outstanding limit

--- src/main/java/com/google/cloud/pubsublite/flink/internal/sink/MessagePublisher.java
+++ src/main/java/com/google/cloud/pubsublite/flink/internal/sink/MessagePublisher.java
@@ -22,21 +22,38 @@ import com.google.cloud.pubsublite.MessageMetadata;
 import com.google.cloud.pubsublite.internal.CheckedApiException;
 import com.google.cloud.pubsublite.internal.ExtractStatus;
 import com.google.cloud.pubsublite.internal.Publisher;
+import com.google.cloud.pubsublite.internal.wire.SystemExecutors;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class MessagePublisher implements BulkWaitPublisher<Message> {
   private final Publisher<MessageMetadata> publisher;
   private final List<ApiFuture<MessageMetadata>> publishes;
+
+  private static final int maxBytesOutstanding = 10*1024*1024;
+  private final Semaphore bytesOutstanding = new Semaphore(maxBytesOutstanding);
 
   public MessagePublisher(Publisher<MessageMetadata> publisher) {
     this.publisher = publisher;
     this.publishes = new ArrayList<>();
   }
 
+  private int getAccountedSize(Message message) {
+    long size = message.toProto().getSerializedSize();
+    if (size > maxBytesOutstanding) {
+      return maxBytesOutstanding;
+    }
+    return Math.toIntExact(size);
+  }
+
   @Override
-  public void publish(Message message) {
-    publishes.add(publisher.publish(message));
+  public void publish(Message message) throws InterruptedException {
+    final int size = getAccountedSize(message);
+    bytesOutstanding.acquire(size);
+    ApiFuture<MessageMetadata> future = publisher.publish(message);
+    future.addListener(() -> bytesOutstanding.release(size), SystemExecutors.getAlarmExecutor());
+    publishes.add(future);
   }
 
   @Override
