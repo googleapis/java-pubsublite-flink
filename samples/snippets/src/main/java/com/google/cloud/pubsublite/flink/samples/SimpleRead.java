3eb1f1b51c9b2db0df5b07970fd6b085012ae926
palmere <palmere@google.com>
2021-10-01 10:11:23 -0400

    formatting

--- samples/snippets/src/main/java/com/google/cloud/pubsublite/flink/samples/SimpleRead.java
+++ samples/snippets/src/main/java/com/google/cloud/pubsublite/flink/samples/SimpleRead.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.cloud.pubsublite.flink.samples;
 
 import com.google.cloud.pubsublite.SequencedMessage;
